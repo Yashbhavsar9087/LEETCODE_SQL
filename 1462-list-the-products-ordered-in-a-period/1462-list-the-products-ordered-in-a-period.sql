6bf4f5a2234a0fd9fcd904304339cc3bbdd4b4ce
Yashbhavsar9087 <65526108+Yashbhavsar9087@users.noreply.github.com>
2025-07-30 16:52:17 -0500

    Time: 1268 ms (5.06%), Space: 0B (100%) - LeetHub

--- 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -2,6 +2,6 @@
 select product_name, sum(unit) as unit 
 from products p 
 join orders o on p.product_id = o.product_id
-where year(order_date) = 2020 and month(order_date) = 02
+where format(order_date, 'MM/yyyy') = '02/2020'
 group by o.product_id, product_name
 having sum(unit) >= 100
