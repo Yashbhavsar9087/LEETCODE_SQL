1c154bdb549eb7a97a6c1a8cee084a5af19ff102
Yashbhavsar9087 <65526108+Yashbhavsar9087@users.noreply.github.com>
2025-08-08 18:25:51 -0500

    Time: 561 ms (84.29%), Space: 0B (100%) - LeetHub

---
+++ 3865-find-product-recommendation-pairs/3865-find-product-recommendation-pairs.sql
@@ -0,0 +1,20 @@
+# Write your MySQL query statement below
+with cte as (
+    select user_id, product_id
+    from productpurchases
+),
+
+cte2 as (
+    select c1.user_id, c1.product_id as product1_id, c2.product_id as product2_id  from cte c1 join cte c2 on c1.user_id = c2.user_id and c1.product_id < c2.product_id
+),
+
+cte3 as (
+    select count(*) as customer_count , product1_id, product2_id  from cte2
+    group by product1_id, product2_id 
+    having count(*) >= 3
+)
+
+select product1_id, product2_id, pro.category as product1_category, pro1.category as product2_category,  customer_count from cte3
+left join ProductInfo pro on cte3.product1_id = pro.product_id
+left join ProductInfo pro1 on cte3.product2_id = pro1.product_id
+order by customer_count desc, product1_id, product2_id
