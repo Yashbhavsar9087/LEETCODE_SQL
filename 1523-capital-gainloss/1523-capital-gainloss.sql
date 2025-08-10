0910e03d0ee1858dacce098938bcb22c7bd83635
Yashbhavsar9087 <65526108+Yashbhavsar9087@users.noreply.github.com>
2025-08-09 20:01:22 -0500

    Time: 557 ms (27.94%), Space: 0B (100%) - LeetHub

--- 1523-capital-gainloss/1523-capital-gainloss.sql
+++ 1523-capital-gainloss/1523-capital-gainloss.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select stock_name, sum(new_price) as capital_gain_loss  from (select stock_name, CASE WHEN operation = 'Sell' then price ELSE -price END as new_price
-from stocks) t
+select stock_name, sum(CASE WHEN operation = 'Sell' then price ELSE -price END) as capital_gain_loss
+from stocks
 group by stock_name
